a3a4440b93001be8f5f5da7642c830215691fed7
re-ovo <1609403959@qq.com>
2025-11-27 18:04:08 +0800

    chore: update dependencies and refactor App component structure

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from "vite-plugin-wasm";
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({

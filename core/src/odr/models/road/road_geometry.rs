737d6f5bd0396bdb8d385ea76e900ad91753a2e3
re-ovo <1609403959@qq.com>
2025-11-29 14:33:03 +0800

    feat: refactor OdrRoadGeometry struct to include new properties and change geometry type to kind enum

--- core/src/odr/models/road/road_geometry.rs
+++ core/src/odr/models/road/road_geometry.rs
@@ -1,71 +1,65 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::math::vec2::Vec2;
-
 #[wasm_bindgen]
 #[derive(Clone, Debug)]
 pub struct OdrRoadGeometry {
-    s: f64,
-    x: f64,
-    y: f64,
-    hdg: f64,
-    length: f64,
-    geometry_type: OdrRoadGeometryType,
+    pub s: f64,
+    pub x: f64,
+    pub y: f64,
+    pub hdg: f64,
+    pub length: f64,
+    pub kind: OdrRoadGeometryKind,
+
+    // Spiral 字段
+    #[wasm_bindgen(js_name = "curvStart")]
+    pub curv_start: Option<f64>,
+    #[wasm_bindgen(js_name = "curvEnd")]
+    pub curv_end: Option<f64>,
+
+    // Arc 字段
+    pub curvature: Option<f64>,
+
+    // ParamPoly3 字段
+    #[wasm_bindgen(js_name = "aU")]
+    pub a_u: Option<f64>,
+    #[wasm_bindgen(js_name = "aV")]
+    pub a_v: Option<f64>,
+    #[wasm_bindgen(js_name = "bU")]
+    pub b_u: Option<f64>,
+    #[wasm_bindgen(js_name = "bV")]
+    pub b_v: Option<f64>,
+    #[wasm_bindgen(js_name = "cU")]
+    pub c_u: Option<f64>,
+    #[wasm_bindgen(js_name = "cV")]
+    pub c_v: Option<f64>,
+    #[wasm_bindgen(js_name = "dU")]
+    pub d_u: Option<f64>,
+    #[wasm_bindgen(js_name = "dV")]
+    pub d_v: Option<f64>,
+    #[wasm_bindgen(js_name = "pRange")]
+    pub p_range: Option<OdrParamPoly3PRange>,
 }
 
-#[derive(Clone, Debug)]
-pub enum OdrRoadGeometryType {
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum OdrRoadGeometryKind {
     Line,
-    Spiral {
-        curv_start: f64,
-        curv_end: f64,
-    },
-    Arc {
-        curvature: f64,
-    },
-    ParamPoly3 {
-        a_u: f64,
-        a_v: f64,
-        b_u: f64,
-        b_v: f64,
-        c_u: f64,
-        c_v: f64,
-        d_u: f64,
-        d_v: f64,
-        p_range: OdrParamPoly3PRange,
-    },
+    Spiral,
+    Arc,
+    ParamPoly3,
 }
 
 /// Enumerations of the paramPoly3 pRange attribute
 #[wasm_bindgen]
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug)]
 pub enum OdrParamPoly3PRange {
     ArcLength,
     Normalized,
 }
 
-impl OdrRoadGeometry {
-    pub fn new(
-        s: f64,
-        x: f64,
-        y: f64,
-        hdg: f64,
-        length: f64,
-        geometry_type: OdrRoadGeometryType,
-    ) -> Self {
-        Self {
-            s,
-            x,
-            y,
-            hdg,
-            length,
-            geometry_type,
-        }
-    }
-}
-
 #[wasm_bindgen]
 impl OdrRoadGeometry {
+    #[wasm_bindgen(js_name = "createLine")]
     pub fn create_line(s: f64, x: f64, y: f64, hdg: f64, length: f64) -> Self {
         Self {
             s,
@@ -73,10 +67,23 @@ impl OdrRoadGeometry {
             y,
             hdg,
             length,
-            geometry_type: OdrRoadGeometryType::Line,
+            kind: OdrRoadGeometryKind::Line,
+            curv_start: None,
+            curv_end: None,
+            curvature: None,
+            a_u: None,
+            a_v: None,
+            b_u: None,
+            b_v: None,
+            c_u: None,
+            c_v: None,
+            d_u: None,
+            d_v: None,
+            p_range: None,
         }
     }
 
+    #[wasm_bindgen(js_name = "createSpiral")]
     pub fn create_spiral(
         s: f64,
         x: f64,
@@ -92,13 +99,23 @@ impl OdrRoadGeometry {
             y,
             hdg,
             length,
-            geometry_type: OdrRoadGeometryType::Spiral {
-                curv_start,
-                curv_end,
-            },
+            kind: OdrRoadGeometryKind::Spiral,
+            curv_start: Some(curv_start),
+            curv_end: Some(curv_end),
+            curvature: None,
+            a_u: None,
+            a_v: None,
+            b_u: None,
+            b_v: None,
+            c_u: None,
+            c_v: None,
+            d_u: None,
+            d_v: None,
+            p_range: None,
         }
     }
 
+    #[wasm_bindgen(js_name = "createArc")]
     pub fn create_arc(s: f64, x: f64, y: f64, hdg: f64, length: f64, curvature: f64) -> Self {
         Self {
             s,
@@ -106,10 +123,23 @@ impl OdrRoadGeometry {
             y,
             hdg,
             length,
-            geometry_type: OdrRoadGeometryType::Arc { curvature },
+            kind: OdrRoadGeometryKind::Arc,
+            curv_start: None,
+            curv_end: None,
+            curvature: Some(curvature),
+            a_u: None,
+            a_v: None,
+            b_u: None,
+            b_v: None,
+            c_u: None,
+            c_v: None,
+            d_u: None,
+            d_v: None,
+            p_range: None,
         }
     }
 
+    #[wasm_bindgen(js_name = "createParamPoly3")]
     pub fn create_param_poly3(
         s: f64,
         x: f64,
@@ -132,21 +162,19 @@ impl OdrRoadGeometry {
             y,
             hdg,
             length,
-            geometry_type: OdrRoadGeometryType::ParamPoly3 {
-                a_u,
-                a_v,
-                b_u,
-                b_v,
-                c_u,
-                c_v,
-                d_u,
-                d_v,
-                p_range,
-            },
+            kind: OdrRoadGeometryKind::ParamPoly3,
+            curv_start: None,
+            curv_end: None,
+            curvature: None,
+            a_u: Some(a_u),
+            a_v: Some(a_v),
+            b_u: Some(b_u),
+            b_v: Some(b_v),
+            c_u: Some(c_u),
+            c_v: Some(c_v),
+            d_u: Some(d_u),
+            d_v: Some(d_v),
+            p_range: Some(p_range),
         }
-    }
-
-    pub fn get_xy(&self, s: f64) -> Vec2 {
-        return Vec2::default();
     }
 }

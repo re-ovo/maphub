2bf3ceaa7b956d47807171fc9131c725c90d19fe
re-ovo <re_dev@qq.com>
2025-12-02 23:26:39 +0800

    refacor: 改进element设计

--- web/src/viewer/types/element.ts
+++ web/src/viewer/types/element.ts
@@ -1,4 +1,5 @@
 import type { Id } from "@/utils/id";
+import type { ReactNode } from "react";
 
 export type ElementType = string;
 
@@ -10,4 +11,36 @@ export interface ElementNode<D, T extends ElementType> {
   visible: boolean;
   type: T;
   data: D;
+  provideHoverInfo(): HoverInfo | null;
+  provideProperties(): PropertyGroup[] | null;
+  provideTree(): TreeAction[] | null;
+}
+
+export type Value = string | number | ReactNode | null;
+
+export type HoverInfo = {
+  title: string;
+  subtitle: string;
+  items: HoverInfoItem[];
+};
+
+export type HoverInfoItem = {
+  label: string;
+  value: Value;
+};
+
+export interface PropertyGroup {
+  label: string;
+  properties: PropertyItem[];
+}
+
+export interface PropertyItem {
+  label: string;
+  value: Value;
+}
+
+export interface TreeAction {
+  icon?: ReactNode;
+  label: string | ReactNode;
+  onClick: () => void;
 }

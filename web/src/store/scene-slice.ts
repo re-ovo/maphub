60cf0705b610f216855e7117411f3259330cb3e2
re-ovo <re_dev@qq.com>
2025-12-04 22:25:01 +0800

    refactor: 移除老的设计

--- web/src/store/scene-slice.ts
+++ web/src/store/scene-slice.ts
@@ -1,10 +1,8 @@
 import type { Id } from "@/utils/id";
-import type { ElementNode } from "@/viewer/types/element";
 import type { ReactNode } from "react";
 import type { StateCreator } from "zustand";
 
 export interface SceneSlice {
-  elements: ElementNode[];
   hoverData: HoverData | null;
 }
 
@@ -14,16 +12,7 @@ export const createSceneSlice: StateCreator<SceneSlice, [], [], SceneSlice> = (
 ) => ({
   elements: [],
   hoverData: null,
-
-  toggleElementVisibility: (id: Id) => {
-    set((state) => ({
-      elements: state.elements.map((element) =>
-        element.id === id ? { ...element, visible: !element.visible } : element
-      ),
-    }));
-
-    // TODO: update renderer?
-  },
+  setHoverData: (data: HoverData | null) => set({ hoverData: data }),
 });
 
 interface HoverData {

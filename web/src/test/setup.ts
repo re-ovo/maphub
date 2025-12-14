6b20cefa829e644d139d7f9b877c367b27fa0e2e
re-ovo <1609403959@qq.com>
2025-12-14 23:36:16 +0800

    feat: add jsdom dependency for improved DOM manipulation and testing capabilities

--- web/src/test/setup.ts
+++ web/src/test/setup.ts
@@ -1,10 +1,1 @@
 import { expect, afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-
-// 自动清理 React 组件
-afterEach(() => {
-  cleanup();
-});
-
-// 可以在这里添加全局的测试设置
-// 例如：模拟浏览器 API、设置全局变量等
